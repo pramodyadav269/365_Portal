1f46edcc92031ec4de7f0cac827ec71021cc15c9
Imtiyaz Khan <Imtiyazkhan.xbiz@outlook.com>
2019-10-24 23:42:26 +0530

    validation done
    check validationform page all input type validation

--- 365_Portal/Asset/js/site.js
+++ 365_Portal/Asset/js/site.js
@@ -29,3 +29,59 @@ function toggle(view, hide) {
     $('#' + view).removeClass('d-none');
     $('#' + hide).addClass('d-none');
 }
+
+function inputValidation(container) {
+
+    $(container).find('.required').each(function (i, _input) {
+        var input = $(_input);
+
+        var val = $(input).val();
+        if (input.attr('type') === 'text' || input.attr('type') === 'password' ||
+            input.attr('type') === 'number' || input.attr('type') === 'email' || input.prop("tagName") === "TEXTAREA") {
+            input.removeClass('is-invalid').removeClass('is-valid');
+            if (val === undefined || val === null || val === "") {
+                input.addClass('is-invalid');
+            }
+            else {
+                input.addClass('is-valid');
+            }
+        }
+
+        if (input.attr('type') === 'file') {
+            input.removeClass('is-invalid').removeClass('is-valid');
+            if (val === undefined || val === null || val === "") {
+                input.addClass('is-invalid');
+            }
+            else {
+                input.addClass('is-valid');
+            }
+        }
+
+        if (input.prop("tagName") === "SELECT") {
+            input.next().removeClass('is-invalid').removeClass('is-valid');
+            if (val !== undefined && val !== null && val !== "" && val.length > 0) {
+                input.next().addClass('is-valid');
+            }
+            else {
+                input.next().addClass('is-invalid');
+            }
+        }
+
+        if (input.hasClass('radio') || input.hasClass('checkbox')) {
+            input.find('input').removeClass('is-invalid').removeClass('is-valid');
+            if (input.find('input').is(':checked')) {
+
+                input.find('input').addClass('is-valid');
+            }
+            else {
+                input.find('input').addClass('is-invalid');
+            }
+        }
+    });
+
+    if ($(container).find('.is-invalid').length < 1) {
+        return true;
+    } else {
+        return false;
+    }
+}
