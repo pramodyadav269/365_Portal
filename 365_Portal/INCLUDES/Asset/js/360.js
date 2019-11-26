6b6e481552c3f675078504caac4e28d9e500523a
Imtiyaz Khan <Imtiyazkhan.xbiz@outlook.com>
2019-11-27 00:42:09 +0530

    topics changes and master page sidenav

--- 365_Portal/INCLUDES/Asset/js/360.js
+++ 365_Portal/INCLUDES/Asset/js/360.js
@@ -30,11 +30,13 @@ $(document).ready(function () {
         $(this).addClass('active');
 
         if ($(this).hasClass('side-menu')) {
-            $('.sidenav-content .sidenav-item').find('.sidenav-link span').css({ "opacity": "0" });
+            $('.sidenav-content .sidenav-item').find('.sidenav-link').addClass('cu-tooltiptext');
+            $('.sidenav-content .sidenav-item').find('.sidenav-link span:not(.tooltiptext)').css({ "opacity": "0" });
             $('.sidenav-content-menu').css({ "transform": "translate3d(52px, 0, 0)" });
 
         } else {
-            $('.sidenav-content .sidenav-item').find('.sidenav-link span').css({ "opacity": "100" });
+            $('.sidenav-content .sidenav-item').find('.sidenav-link').removeClass('cu-tooltiptext');
+            $('.sidenav-content .sidenav-item').find('.sidenav-link span:not(.tooltiptext)').css({ "opacity": "100" });
             $('.sidenav-content-menu').css({ "transform": "translate3d(240px, 0, 0)" });
 
 
