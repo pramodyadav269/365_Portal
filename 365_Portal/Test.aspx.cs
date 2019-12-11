716369a036e18fddc8babd30929dd84337a7b4ca
SujeetKumar <SujeetKumar@DESKTOP-EHF26GH>
2019-12-11 23:33:46 +0530

    Removed key code of SendGrid Email API

--- 365_Portal/Test.aspx.cs
+++ 365_Portal/Test.aspx.cs
@@ -10,33 +10,7 @@ namespace _365_Portal
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            Run();
-        }
-    
-        static void Run()
-        {
-            //var dd = Environment.GetEnvironmentVariables();
-            //var apiKey = Environment.GetEnvironmentVariable("SENDGRID_API_KEY");
-            var client = new SendGridClient("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");
-            var from = new EmailAddress("yogtambe.it@gmail.com", "Example User");
-            var subject = "Sending with SendGrid is Fun";
-            var to = new EmailAddress("yogtambe.it@gmail.com", "Example User");
-            var plainTextContent = "Run and easy to do anywhere, even with C# " + DateTime.Now;
-            var htmlContent = "<strong>and easy to do anywhere, even with C#</strong>";
-            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
-            var response = client.SendEmailAsync(msg);
-        }
-
-        static void SendEmail()
-        {
-            var client = new SendGridClient(WebConfigurationManager.AppSettings["SendGridEmailKey"]);
-            var from = new EmailAddress("yogtambe.it@gmail.com", "Example User");
-            var subject = "Sending with SendGrid is Fun";
-            var to = new EmailAddress("yogtambe.it@gmail.com", "Example User");
-            var plainTextContent = "Run and easy to do anywhere, even with C# " + DateTime.Now;
-            var htmlContent = "<strong>and easy to do anywhere, even with C#</strong>";
-            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
-            var response = client.SendEmailAsync(msg);
+            
         }
     }
 }
